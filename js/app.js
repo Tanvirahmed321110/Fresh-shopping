28637cd810111a8d6177e02fa6e7840c374917a5
tanvir321110 <tanvirahmed321110@gmail.com>
2024-02-12 15:35:53 +0600

    header top is ok

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+//  language drop down
+const languageDropdownBtn = document.getElementById('language-dropdown-btn');
+const languageDropdown = document.getElementById('language-dropdown')
+
+languageDropdownBtn.addEventListener('click',()=>{
+    languageDropdown.classList.toggle('active')
+})
