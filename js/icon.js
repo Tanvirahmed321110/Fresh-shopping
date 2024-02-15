97c03dc788b998e24b1cfc145b028454eb5a3e3a
tanvir321110 <tanvirahmed321110@gmail.com>
2024-02-15 22:25:09 +0600

    home page ok

--- js/icon.js
+++ js/icon.js
@@ -8,5 +8,11 @@ const girdIcon = `
     <rect x="3" y="14" width="7" height="7"></rect>
     </svg>
 `
-
+const recipesIcon = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-journal text-primary" viewBox="0 0 16 16">
+    <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"></path>
+    <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"></path>
+    </svg>
+`
 document.getElementById('grid-icon').innerHTML= girdIcon
+document.getElementById('recipes-icon').innerHTML = recipesIcon
