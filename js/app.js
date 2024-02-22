9b10d4505ae202c7574ef7aaa423288691a782db
akjmalamin7 <thpi205526@gmail.com>
2024-02-22 18:34:22 +0600

    create update modal

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
-
 //  language drop down
-const languageDropdownBtn = document.getElementById('language-dropdown-btn');
-const languageDropdown = document.getElementById('language-dropdown')
+const languageDropdownBtn = document.getElementById("language-dropdown-btn");
+const languageDropdown = document.getElementById("language-dropdown");
 
-languageDropdownBtn.addEventListener('click',()=>{
-    languageDropdown.classList.toggle('active')
-})
-
-
-
-
-
-
+languageDropdownBtn.addEventListener("click", () => {
+    languageDropdown.classList.toggle("active");
+});
 
 // cart modal
+const overlay = document.getElementById("overlay");
+const cartModal = document.getElementById("cart-modal");
+const cardModalContent = document.getElementById("card_modal_content");
+const bodyTag = document.getElementsByTagName("body")[0];
 
-const cartModal = document.getElementById("cart-modal")
+document.getElementById("btn-cart").addEventListener("click", function () {
+    /* overlay */
+    overlay.classList.add("active");
+    cardModalContent.classList.add("active");
+    bodyTag.style.overflowY = "hidden";
+});
 
-document.getElementById('btn-cart').addEventListener('click',function(){
-    cartModal.classList.toggle('active');
-    cartModal.style.display='block'
-})
-
-document.getElementById('cart-modal-close').addEventListener('click',function(){
-    cartModal.style.display='none'
-})
+document
+    .getElementById("cart-modal-close")
+    .addEventListener("click", function () {
+        overlay.classList.remove("active");
+        cardModalContent.classList.remove("active");
+        bodyTag.style.overflowY = "visible";
+    });
