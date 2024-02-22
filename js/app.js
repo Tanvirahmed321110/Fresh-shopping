3d244b33fd0e37153267dab348521fb161615eb2
tanvir321110 <tanvirahmed321110@gmail.com>
2024-02-22 17:03:20 +0600

    cart modal completed

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@ const languageDropdown = document.getElementById('language-dropdown')
 languageDropdownBtn.addEventListener('click',()=>{
     languageDropdown.classList.toggle('active')
 })
+
+
+
+
+
+
+
+// cart modal
+
+const cartModal = document.getElementById("cart-modal")
+
+document.getElementById('btn-cart').addEventListener('click',function(){
+    cartModal.classList.toggle('active');
+    cartModal.style.display='block'
+})
+
+document.getElementById('cart-modal-close').addEventListener('click',function(){
+    cartModal.style.display='none'
+})
