35bf3459d4dcf6fd7f406115f2147cd16b257bac
tanvir321110 <tanvirahmed321110@gmail.com>
2024-02-24 02:42:38 +0600

    product page start

--- js/app.js
+++ js/app.js
@@ -9,8 +9,31 @@ languageDropdownBtn.addEventListener('click',()=>{
 
 
 
+// Select all small images
+const smallImages = [...document.querySelectorAll('.small-image img')];
+
+// Loop through each small image and attach a click event listener
+smallImages.forEach((eachImage) => {
+    eachImage.addEventListener('click', function() {
+        // remove in active class
+        smallImages.forEach((child) => {
+            child.classList.remove('active');
+        });
+
+        // Set the 'src' attribute of the big image to the clicked small image's 'src'
+        document.getElementById('big-image').src = eachImage.src;
+        // add active class
+        eachImage.classList.add('active');
+    });
+});
 
 
+
+// share dropdown
+document.getElementById('share-dropdown-btn').addEventListener('click',function(){
+    document.getElementById('share-dropdown').classList.toggle('active');
+    document.getElementById('share-dropdown-btn').classList.toggle('active')
+})
 
 
 // cart modal
